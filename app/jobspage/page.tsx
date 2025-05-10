a623e8818c97d445ed3cfdfdc019e94aa30de103
Kelvin Muriuki <105153920+Muriuk@users.noreply.github.com>
2025-05-10 20:18:49 +0300

    Update page.tsx

--- app/jobspage/page.tsx
+++ app/jobspage/page.tsx
@@ -1,4 +1,4 @@
-import JobspageForm from "../ui/jobspagejob/form"; // Ensure the component name follows Pascal case
+import JobspageForm from "../ui/jobspage/form"; // Ensure the component name follows Pascal case
 
 export default function PostjobPage() {
     return <JobspageForm />;
