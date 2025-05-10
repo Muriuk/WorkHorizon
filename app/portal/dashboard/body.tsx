d78bdc4251de6a933903706818058c9b3698296e
Kelvin Muriuki <105153920+Muriuk@users.noreply.github.com>
2025-05-10 13:19:38 +0300

    Update body.tsx

--- app/portal/dashboard/body.tsx
+++ app/portal/dashboard/body.tsx
@@ -2,6 +2,7 @@
 
 import DashboardBody from '@/app/ui/portal/dashboard';
 
-export default function DashboardMainBody() {
+export default function DashboardBody({ user }: { user: User | null }) {
+
     return <DashboardBody />;
 }
