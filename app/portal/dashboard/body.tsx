e0b2c62144fbf708a2c445c22fdfd7889efb5b0c
Kelvin Muriuki <105153920+Muriuk@users.noreply.github.com>
2025-05-10 13:10:36 +0300

    Update body.tsx

--- app/portal/dashboard/body.tsx
+++ app/portal/dashboard/body.tsx
@@ -1,22 +1,7 @@
 'use client'
-import { User } from '@/app/lib/elements';
+
 import DashboardBody from '@/app/ui/portal/dashboard';
-import { useEffect, useState } from 'react';
 
 export default function DashboardMainBody() {
-    const [user, setUser] = useState<User | null>(null);
-
-    useEffect(() => {
-        const mockUser: User = {
-            id: '1',
-            email: 'admin@example.com',
-            name: 'Admin User',
-            gender: 'Male',
-            post: 'Developer',
-        };
-
-        setUser(mockUser);
-    }, []);
-
-    return <DashboardBody user={user} />;
+    return <DashboardBody />;
 }
