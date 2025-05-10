10d172c1fa6caca2b333292a74b37039e941c356
Kelvin Muriuki <105153920+Muriuk@users.noreply.github.com>
2025-05-10 11:45:16 +0300

    Update section1.tsx

--- app/ui/home/section1.tsx
+++ app/ui/home/section1.tsx
@@ -110,7 +110,7 @@ export default function HomeHead() {
       Post a Job
     </button>
   </Link>
-  <Link href="/register">
+  <Link href="/portal/dashboard">
     <button className="bg-sky-900 hover:bg-sky-800 text-white font-medium py-2 px-5 rounded-full shadow-md transition-transform hover:scale-105 text-sm sm:text-base flex items-center gap-2">
       <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
