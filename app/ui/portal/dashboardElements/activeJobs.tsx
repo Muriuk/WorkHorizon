264549f1068dab041c26ad92b521bedc35a3694b
Kelvin Muriuki <105153920+Muriuk@users.noreply.github.com>
2025-05-10 14:02:13 +0300

    Update activeJobs.tsx

--- app/ui/portal/dashboardElements/activeJobs.tsx
+++ app/ui/portal/dashboardElements/activeJobs.tsx
@@ -22,10 +22,10 @@ export default function ActiveJobs(){
     },[])
 
     return(
-        <div className='w-[85%] my-4 bg-gray-200 rounded-3xl p-8 mx-auto shadow-xl shadow-gray-300'>
+        <div className='w-[90%] sm:w-[85%] my-4 bg-gray-200 rounded-3xl p-4 sm:p-8 mx-auto shadow-xl shadow-gray-300'>
             <div className='bg-gray-100 h-full flex flex-col items-center rounded-3xl py-3'>  
-                <h3 className="w-fit mx-auto mt-2 text-2xl font-semibold text-sky-900 border-b border-orange-500">Jobs Active</h3>
-                <div className='mx-4 mt-4 w-[88%] min-h-[200px] 2xl:min-h-[250px]'>
+                <h3 className="w-fit mx-auto mt-2 text-2xl sm:text-3xl font-semibold text-sky-900 border-b border-orange-500">Jobs Active</h3>
+                <div className='mx-4 mt-4 w-[100%] min-h-[200px] 2xl:min-h-[250px]'>
                     {
                         loading ? 
                         <div className="w-full h-full flex flex-col justify-center items-center ">
@@ -33,11 +33,11 @@ export default function ActiveJobs(){
                         </div>:
                         activeJobs.length > 0 ?                         
                         activeJobs.map((job) => 
-                            <div key={job.jobid} className="grid items-center border-b border-gray-200 text-lg mb-1 grid-cols-[13%,87%]">
+                            <div key={job.jobid} className="grid items-center border-b border-gray-200 text-lg mb-1 grid-cols-1 sm:grid-cols-[13%,87%]">
                                 <h4 className="py-2 px-3 text-orange-500 "><ArrowRightSquare /> </h4>
                                 <Link href={`/portal/dashboard/jobs-list/whjob_${job.jobid}`} className="py-2 hover:text-sky-900 hover:underline transition-all ease-in-out duration-300 hover:scale-[1.02] ">{job.title}</Link>
                             </div>
-                        ):
+                        ) :
                         <div className='flex flex-col items-center justify-center h-[220px] w-full'>
                             <h2 className="text-lg lg:text-xl text-gray-400 font-semibold capitalize text-center">No active job</h2>
                         </div>  
