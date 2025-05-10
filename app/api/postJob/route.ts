58951b77bc4b01f32da8c6ab4ccd7aa0ef79fb7a
Kelvin Muriuki <105153920+Muriuk@users.noreply.github.com>
2025-05-10 18:54:24 +0300

    Update route.ts

--- app/api/postJob/route.ts
+++ app/api/postJob/route.ts
@@ -3,10 +3,10 @@ import mysql from 'mysql2/promise';
 
 async function getConnection() {
   return await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+   host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   });
 }
 
