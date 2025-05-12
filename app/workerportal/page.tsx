5c37358c0bf883d69b86e814269e576e8938e99a
Kelvin Muriuki <105153920+Muriuk@users.noreply.github.com>
2025-05-12 14:06:50 +0300

    Update page.tsx

--- app/workerportal/page.tsx
+++ app/workerportal/page.tsx
@@ -1,5 +1,8 @@
+// workerportal/page.tsx
 import WorkerportalForm from "../ui/workerportal/form"; // path to client component
 
 export default function WorkerportalPage() {
-    return <WorkerportalForm />;
+  const mockEmail = "testuser@example.com"; // Placeholder for now
+
+  return <WorkerportalForm email={mockEmail} />;
 }
