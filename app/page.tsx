f1cc8687b628d429083eb3105dc31df36a1daf10
Kelvin Muriuki <105153920+Muriuk@users.noreply.github.com>
2025-05-14 22:45:17 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,13 @@ import JoinGlobalTeam from './ui/home/joinglobalteam';
 import CareerOpportunities from './ui/home/career';
 import HiringProcess from './ui/home/hiringprocess';
 import LetsTalk from './ui/home/letstalk';
-import Jobspage from './ui/jobspage';
+
 export default function Home() {
 
   return (
     <>
       <HomeHead />
-      <Jobspage />
+     
       <HiringProcess />
       <AboutSection />
       <WhyChooseUs />
