83d12873432d4152ab9dea9da4b37afdc2b085b9
Kelvin Muriuki <105153920+Muriuk@users.noreply.github.com>
2025-05-15 12:32:09 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@
 import HomeHead from './ui/home/section1';
 // import { useEffect, useState } from 'react';
 import  AboutSection  from './ui/home/about';
-import  WhyChooseUs  from './ui/home/choose';
+
 import JoinGlobalTeam from './ui/home/joinglobalteam';
-import CareerOpportunities from './ui/home/career';
 import HiringProcess from './ui/home/hiringprocess';
 import LetsTalk from './ui/home/letstalk';
 export default function Home() {
@@ -14,9 +13,9 @@ export default function Home() {
       <HomeHead />
       <HiringProcess />
       <AboutSection />
-      <WhyChooseUs />
+      
       <JoinGlobalTeam />
-      <CareerOpportunities />
+     
         <LetsTalk />
     </>
   );
