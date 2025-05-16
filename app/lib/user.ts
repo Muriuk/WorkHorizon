eb882cd3414b327600f0cc78b622a90c76d47155
Kelvin Muriuki <105153920+Muriuk@users.noreply.github.com>
2025-05-16 20:31:48 +0300

    Delete app/lib/user.ts

--- app/lib/user.ts
+++
@@ -1,9 +0,0 @@
-import { executeQuery } from "@/lib/db";
-
-export async function getUserById(userId: number) {
-  const result = await executeQuery("SELECT * FROM users WHERE id = ?", [userId]);
-
-  const user = Array.isArray(result) ? result[0] : null;
-
-  return user ?? null;
-}
