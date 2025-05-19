bcb26678af9fb9b0d6ac78b5c7f3eabaddc6ae0d
Kelvin Muriuki <105153920+Muriuk@users.noreply.github.com>
2025-05-19 22:31:31 +0300

    Create page.tsx

---
+++ app/clientregister/page.tsx
@@ -0,0 +1,5 @@
+import ClientRegisterForm from "../ui/clientregister/form"; // Ensure the component name follows Pascal case
+
+export default function clientregisterPage() {
+    return <ClientRegisterForm />;
+}
