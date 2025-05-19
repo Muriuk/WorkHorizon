aa42d8133972644dc5418383929ad456cf6bf9a7
Kelvin Muriuki <105153920+Muriuk@users.noreply.github.com>
2025-05-19 13:46:13 +0300

    Update auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -2,54 +2,56 @@ import type { NextAuthConfig } from 'next-auth';
  
 export const authConfig: NextAuthConfig = {
     secret: process.env.AUTH_SECRET,
-    pages:{
-        signIn: '/portal',
+    pages: {
+        signIn: '/portal',  // Changed from '/portal' to '/' for worker login
         signOut: '/',
     },
-    session:{
+    session: {
         strategy: 'jwt',
-        maxAge: 60*60*24, // 60sec x 5
-        updateAge: 60*60, //60secs
+        maxAge: 60 * 60 * 24, // 24 hours
+        updateAge: 60 * 60, // 1 hour
     },
     
-    callbacks:{
-        session: async({ session, token })=> {
-            if(token){
-                // console.log(token)
+    callbacks: {
+        session: async({ session, token }) => {
+            if (token) {
                 session.user = {
                     ...session.user,
                     name: token.name || '',
                     email: token.email || '',
                     id: typeof token.id === 'string' ? token.id : '',
+                    worker_category: token.worker_category || '',
+                    is_verified: token.is_verified || false
                 }
             }
             return session;
         },
-        jwt: async( { token, user } )=>{
-            // console.log('user token => ',user)
+        jwt: async({ token, user }) => {
             if (user) {
                 token.id = user?.id;
                 token.name = user?.name;
                 token.email = user?.email;
+                token.worker_category = user?.worker_category;
+                token.is_verified = user?.is_verified;
             }
             return token;
         },
         authorized({ auth, request: { nextUrl } }) {
-
             const isLoggedIn = auth?.user;
-
-            const isOnPortal = nextUrl.pathname === '/portall'
-
-            const isOnDashboard = nextUrl.pathname === '/portall/dashboard'
+            const isOnLoginPage = nextUrl.pathname === '/';
+            const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+            
             if (isOnDashboard) {
               if (isLoggedIn) return true;
               return false; // Redirect unauthenticated users to the login page
             }
-            if (isLoggedIn && isOnPortal) {
-              return Response.redirect(new URL('/portall/dashboard', nextUrl));
+            
+            if (isLoggedIn && isOnLoginPage) {
+              return Response.redirect(new URL('/portal/dashboard', nextUrl));
             }
+            
             return true;
         },
     },
-    providers:[],
-}
+    providers: [],
+};
