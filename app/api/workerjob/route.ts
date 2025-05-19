8b36a64b1b459fe74f0be47299272f640fb0b0d1
Kelvin Muriuki <105153920+Muriuk@users.noreply.github.com>
2025-05-19 14:49:23 +0300

    Update route.ts

--- app/api/workerjob/route.ts
+++ app/api/workerjob/route.ts
@@ -1,3 +1,5 @@
+export const runtime = 'nodejs'; // 👈 Forces use of Node runtime instead of Edge
+
 // app/api/jobs/route.ts
 import { NextResponse } from 'next/server';
 import mysql from 'mysql2/promise';
