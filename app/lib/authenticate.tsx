07d0d8cfa92aad62878f0db870306597055f7055
Kelvin Muriuki <105153920+Muriuk@users.noreply.github.com>
2025-05-19 15:22:41 +0300

    Update authenticate.tsx

--- app/lib/authenticate.tsx
+++ app/lib/authenticate.tsx
@@ -1,36 +1,30 @@
 'use server'
 import { signIn } from "@/auth";
 
-
-export async function authentication( formData: FormData) {
+export async function authentication(formData: FormData) {
   try {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const data: Record<string, any> = {};
     formData.forEach((value, key) => {
       data[key] = value;
     });
-    // console.log('Data from Login Form => ', data);
 
     const result = await signIn('credentials', {
       redirect: false,
       ...data,
     });
 
-    // console.log('SignIn result:', result);
-    if(result?.error)
-    {
+    if(result?.error) {
         if (result?.error === 'Invalid password') {
             return { error: 'From Auth Invalid password' };
-        }else if(result?.error === 'User not found'){
+        } else if(result?.error === 'User not found') {
             return { error: 'From Auth User not found' };  
         }
-        return { error: "Authentication failed" }; // Generic error
+        return { error: "Authentication failed" };
     }
-    
-    return { success: true};
-  } 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  catch (error: any) {
+
+    return { success: true };
+  } catch (error: any) {
     console.error('Authentication error On Authentication.tsx');
     return { error: error.message || 'Authentication failed' };
   }
