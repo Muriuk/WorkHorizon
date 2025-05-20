69ae9aa0a57fd3992dc2d4aada9b02e45d60a9c1
Kelvin Muriuki <105153920+Muriuk@users.noreply.github.com>
2025-05-20 11:22:20 +0300

    Create page.tsx

---
+++ app/clientlogin/page.tsx
@@ -0,0 +1,5 @@
+import ClientLoginForm from "../ui/clientlogin/form"; // Ensure the component name follows Pascal case
+
+export default function clientloginPage() {
+    return <ClientLoginForm />;
+}
