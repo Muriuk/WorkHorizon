912cf29f1915aa1b42caf242115bc6b5585f50f5
Kelvin Muriuki <105153920+Muriuk@users.noreply.github.com>
2025-05-20 12:39:09 +0300

    Create page.tsx

---
+++ app/clientdashboard/page.tsx
@@ -0,0 +1,5 @@
+import ClientDashboardForm from "../ui/clientdashboard/form"; // Ensure the component name follows Pascal case
+
+export default function clientdashboardPage() {
+    return <ClientDashboardForm />;
+}
