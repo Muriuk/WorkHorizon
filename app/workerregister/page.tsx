b2ef5fe6ec666063e81ca6c2bfd1826ca2981b89
Kelvin Muriuki <105153920+Muriuk@users.noreply.github.com>
2025-05-23 20:28:48 +0300

    Create page.tsx

---
+++ app/workerregister/page.tsx
@@ -0,0 +1,5 @@
+import WorkerRegisterForm from "../ui/workerregister/form"; // Ensure the component name follows Pascal case
+
+export default function WorkerregisterPage() {
+    return <WorkerRegisterForm />;
+}
